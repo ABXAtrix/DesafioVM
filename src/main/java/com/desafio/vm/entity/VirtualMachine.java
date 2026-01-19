820bd04a6307b6dedd2a85a7707cb3d8054d01e8
GuilhermeMacedoLL <macedo.lac157@gmail.com>
2026-01-19 19:49:59 -0300

    Entidade de VirtualMachine criada

---
+++ src/main/java/com/desafio/vm/entity/VirtualMachine.java
@@ -0,0 +1,53 @@
+package com.desafio.vm.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import enums.StatusVm;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@Table(name = "VIRTUAL_MACHINE")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VirtualMachine {
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "NOME")
+	private String nome;
+
+	@Column(name = "CPU")
+	private BigDecimal cpu;
+
+	@Column(name = "MEMORIA")
+	private BigDecimal memoria;
+
+	@Column(name = "DISCO")
+	private BigDecimal disco;
+
+	@Column(name = "DATA_CRIACAO")
+	private LocalDateTime dataCriação;
+
+	@Column(name = "STATUS")
+	private StatusVm Status;
+
+}
