7ad825b264b34202ba2d67d07992c5404aa0c049
GuilhermeMacedoLL <macedo.lac157@gmail.com>
2026-01-19 20:15:59 -0300

    Mapeamento da classe VirtualMachineDTO

--- src/main/java/com/desafio/vm/dto/VirtualMachineDTO.java
+++ src/main/java/com/desafio/vm/dto/VirtualMachineDTO.java
@@ -1,5 +1,37 @@
 package com.desafio.vm.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.desafio.vm.enums.StatusVmEnum;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class VirtualMachineDTO {
+
+	private Long id;
+
+	private String nome;
+
+	private BigDecimal cpu;
+
+	private BigDecimal memoria;
+
+	private BigDecimal disco;
+
+	private LocalDateTime dataCriação;
+
+	private StatusVmEnum status;
 
 }
