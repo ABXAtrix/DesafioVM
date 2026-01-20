af0672657177c25d80995e0626a7eb6751c34044
GuilhermeMacedoLL <macedo.lac157@gmail.com>
2026-01-19 23:17:18 -0300

    Correção de relacionamento entre VirtualMachine e Usuario e Anotações para seguir regras de negocio

--- src/main/java/com/desafio/vm/entity/VirtualMachine.java
+++ src/main/java/com/desafio/vm/entity/VirtualMachine.java
@@ -12,7 +12,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -36,15 +43,26 @@ public class VirtualMachine {
 	private Long id;
 
 	@Column(name = "NOME")
+	@NotBlank(message = "O nome é obrigatório")
+	@Size(min = 5, message = "O nome deve ter no mínimo 5 caracteres")
 	private String nome;
 
+	@Min(value = 1, message = "A CPU deve ter pelo menos 1 núcleo")
+	@Positive
 	@Column(name = "CPU")
-	private BigDecimal cpu;
+	@NotNull(message = "A quantidade de núcleos da CPU é obrigatório")
+	private Integer cpu;
 
+	@Min(1)
+	@Positive(message = "A memória deve ser maior que zero")
 	@Column(name = "MEMORIA")
+	@NotNull(message = "A quantidade de memória RAM é obrigatória")
 	private BigDecimal memoria;
 
+	@Min(1)
+	@Positive(message = "O disco deve ser maior que zero")
 	@Column(name = "DISCO")
+	@NotNull(message = "A quantidade de memória do disco")
 	private BigDecimal disco;
 
 	@Column(name = "DATA_CRIACAO")
@@ -53,5 +71,9 @@ public class VirtualMachine {
 	@Column(name = "STATUS")
 	@Enumerated(EnumType.STRING)
 	private StatusVmEnum status;
+
+	@ManyToOne // Indica que muitas VMs pertencem a um único usuário
+	@JoinColumn(name = "USUARIO_ID") // Nome da coluna que será a chave estrangeira no banco
+	private Usuario usuario;
 
 }
