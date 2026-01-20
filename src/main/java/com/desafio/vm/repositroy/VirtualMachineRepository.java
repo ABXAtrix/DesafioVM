4794bb360103c314523568271ce003483fcb2d21
GuilhermeMacedoLL <macedo.lac157@gmail.com>
2026-01-19 21:04:12 -0300

    Mapeamento da classe VirtualMachineRepository

--- src/main/java/com/desafio/vm/repositroy/VirtualMachineRepository.java
+++ src/main/java/com/desafio/vm/repositroy/VirtualMachineRepository.java
@@ -1,8 +1,13 @@
 package com.desafio.vm.repositroy;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import com.desafio.vm.entity.VirtualMachine;
+
 @Repository
-public interface VirtualMachineRepository {
+public interface VirtualMachineRepository
+		extends JpaRepository<VirtualMachine, Long>, JpaSpecificationExecutor<VirtualMachine> {
 
 }
